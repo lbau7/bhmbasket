af6f88f2dda16d274c80dd0b28e9ba805207f885
lbau7 <l.baumann@aon.at>
2024-04-10 10:23:31 +0200

    undo last commit

--- R/AnalysisFunctions.R
+++ R/AnalysisFunctions.R
@@ -205,7 +205,7 @@ getPostQuantiles <- function (
     "qbetaDiff")
   
   ## prepare chunking
-  chunks_outer <- chunkVector(seq_len(n_analyses), 10)
+  chunks_outer <- chunkVector(seq_len(n_analyses), foreach::getDoParWorkers())
   
   "%dorng%" <- doRNG::"%dorng%"
   "%dopar%" <- foreach::"%dopar%"
@@ -217,7 +217,7 @@ getPostQuantiles <- function (
     .packages = c("R2jags"),
     .export   = exported_stuff) %dorng% {
       
-      chunks_inner <- chunkVector(k, 10)
+      chunks_inner <- chunkVector(k, foreach::getDoParWorkers())
       
       foreach::foreach(i = chunks_inner, .combine = c) %do% {
         
